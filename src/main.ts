071414b5bd8e276599264b9a116accb9a0eb3d0a
Sandi <sandisunandar99@gmail.com>
2022-04-14 13:40:49 +0700

    feat/dockerzie

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from "dotenv";
+
+const cc = dotenv.config({ path: ".env" });
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env["PORT"] || 3000);
+  await app.listen(process.env.PORT || 3000);
+
+  console.log(`Application is running on port:  ${await app.getUrl()}`);
+  
 }
 bootstrap();
